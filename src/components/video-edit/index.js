72286a07a4b80c6e6653a34cd48ea9d570ed40ff
howardkitto <kitto.howard@gmail.com>
2019-12-02 20:05:48 +0000

    imorting component

---
+++ src/components/video-edit/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class VideoEdit extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h2>
+                Video Component
+            </h2>
+        )
+    }
+}
+
+export default VideoEdit
