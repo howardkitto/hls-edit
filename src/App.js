d7b68602e3be13308aa7ef1e182d1df9da12dc6b
howardkitto <kitto.howard@gmail.com>
2019-12-03 08:21:08 +0000

    commit before redo of redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import VideoEdit from "./components/video-edit"
 
 
 function App() {
   return (
-    <div >
-      <header >
-   <h1>Hello you</h1>
-   <VideoEdit />
-      </header>
-    </div>
+    
+      <div>
+        <header >
+          <h1>Hello you</h1>
+          <VideoEdit />
+        </header>
+      </div>
+
   );
 }
 
-export default App;
+export default connect()(App);
 
--- src/components/video-edit/index.js
+++ src/components/video-edit/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+const getVideos = state => state.videos.byId;
+
+const getVideoById = id => createSelector(
+    [getVideos],
+    videos => videos[id],
+  );
 
 class VideoEdit extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isManifestLoaded: false,
+            isManifestLoading: false,
+            segments: null,
+            selectedSegments: [],
+            playerUrl: null,
+        };
     }
 
     render() {
+        const { isManifestLoaded, playerUrl } = this.state;
+        const { video } = this.props;
+
+        if (!isManifestLoaded) {
+            return null;
+        }
+
         return (
+
             <h2>
                 Video Component
             </h2>
@@ -14,4 +39,8 @@ class VideoEdit extends React.Component {
     }
 }
 
-export default VideoEdit
+const mapStateToProps = (state, ownProps) => ({
+    video: getVideoById("videFileURL")(state),
+  });
+
+export default connect(mapStateToProps)(VideoEdit)
